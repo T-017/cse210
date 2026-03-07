b899a46051868e1bdf1775c38cef4c50260679c6
T-017 <jholt2@byupathway.edu>
2026-03-07 08:59:02 -0600

    number list

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -3,7 +3,41 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+    {     
+        List<int> numbersList = new List<int>();
+
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter a list of numbers, enter 0 to quit: ");
+
+            string numberEntry = Console.ReadLine();
+            number = int.Parse(numberEntry);
+
+            if (number != 0)
+            {
+                numbersList.Add(number);
+            }
+        }
+        int sum = 0;
+        foreach (int num in numbersList)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+
+        float average = (float)sum / numbersList.Count;
+        Console.WriteLine($"The average of the numbers is: {average}");
+
+        int max = numbersList[0];
+        foreach (int num in numbersList)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        Console.WriteLine($"The maximum number is: {max}");
     }
 }
