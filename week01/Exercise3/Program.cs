ec8369c8cfefba35a467b5149335e1cca03cd0e3
T-017 <jholt2@byupathway.edu>
2026-03-06 22:20:47 -0600

    guess number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int answer = randomGenerator.Next(1, 10);
+
+        int guess = -1;
+
+        while (guess != answer)
+        {
+            Console.Write("Guess a number between 1 and 10: ");;
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < answer)
+            {
+                Console.Write("Higher, try again. ");
+            }
+            else if (guess > answer)
+            {
+                Console.Write("Lower, try again. ");
+            }
+            else
+            {
+                Console.WriteLine("That's correct.");
+            }
+        }
     }
 }
