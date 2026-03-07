c20949b6d6df1ee6ff24f585961e29f99cbc66a2
T-017 <jholt2@byupathway.edu>
2026-03-07 09:30:35 -0600

    name and number

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayOpenMessage();
+
+        string userName = PromptUserName();
+        int userNum = PromptUserNum();
+
+        int squaredNum = SquareNum(userNum);
+
+        DisplayResult(userName, squaredNum);
     }
+
+    static void DisplayOpenMessage()
+    {
+        Console.WriteLine("Welcome to the program.");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNum()
+    {
+        Console.Write("What is your favorite number? ");
+        string userNum = Console.ReadLine();
+        return int.Parse(userNum);
+    }
+
+    static int SquareNum(int userNum)
+    {
+        return userNum * userNum;
+    }
+
+    static void DisplayResult(string userName, int squaredNum)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNum}.");
+    }
+
 }
