e0dcd2620c51e66c0c9122bff27b458c8ccdf534
T-017 <jholt2@byupathway.edu>
2026-03-06 21:27:52 -0600

    else if

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        int x = 5;
+        int y = 2;
+
+        if (x > y)
+        {
+            Console.WriteLine("Greater");
+        }
+        else if (x < y)
+        {
+            Console.WriteLine("Lesser");
+        }
+        else
+        {
+            Console.WriteLine("Equal");
+        }
     }
 }
