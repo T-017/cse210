535bc4a3999d2e4f2ffe2b2015cd2a8f92577a60
T-017 <jholt2@byupathway.edu>
2026-03-11 16:56:11 -0500

    jobs and resumes

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Console.WriteLine("Resumes Project.");
+
+
     }
 }
