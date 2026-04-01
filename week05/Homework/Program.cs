a2ce8361ca3c2dc7c0a2b1b3e05993ca3ca5813c
T-017 <jholt2@byupathway.edu>
2026-03-31 22:10:56 -0500

    homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment a1 = new Assignment("Jane Doe", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("John Doe", "Fractions", "7.3", "1-10");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Sally Doe", "European History", "The Causes of World War II", "May 23, 2024");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
