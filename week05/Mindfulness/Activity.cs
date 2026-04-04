4d8efb9c181868d0f34f3769282bea8079844016
T-017 <jholt2@byupathway.edu>
2026-04-04 14:58:04 -0500

    mindful

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Activity
+{
+  protected string _name;
+  protected string _description;
+  protected int _duration;
+
+  public Activity(string name, string description, int duration)
+  {
+    _name = name;
+    _description = description;
+    _duration = duration;
+  }
+}
